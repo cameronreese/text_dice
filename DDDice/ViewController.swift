3f1d0e21b7b167aec1634531bd6a9399b0c01cbe
Cameron Reese <cameronreese@mac.com>
2016-04-15 18:12:42 -0500

    -Fixed bug that displayed different values for main screen and log screen, they are now the same -Changed algorithm that simulated slowing down randomly selected numbers -Added some media

--- DDDice/ViewController.swift
+++ DDDice/ViewController.swift
@@ -9,6 +9,8 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     var notBreaked = false
     
+    let tickEnd = 8
+    
     var runningSum = 0
     
     @IBOutlet weak var currentRollDisplayLabel: UILabel!
@@ -36,8 +38,8 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     @IBAction func diceButtonPressed(sender: UIButton) {
         
         notBreaked = true
-        
         var numberOfDiceSides: Int
+        var diceRollResult = 0
         
         // determine which dice button was pressed and change numberOfDiceSides appropriately
         if (sender.titleLabel?.text == "d3") {
@@ -59,27 +61,49 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         else {
             numberOfDiceSides = 0 }
         
-        // find a random number based on number of sides
-        let diceRollResult = Int(arc4random_uniform(UInt32(numberOfDiceSides))) + 1
         
-        spinTheWheel(numberOfDiceSides)
+        self.currentRollDisplayLabel.text = String(Int(arc4random_uniform(UInt32(numberOfDiceSides))) + 1)
         
-        currentRollDisplayLabel.text = String(diceRollResult)
-        
-        runningSum += diceRollResult
-        
-        sumLabel.text = "Sum: " + String(runningSum)
-        
-        
-        if let diceLabel = sender.titleLabel?.text! {
-            let currentLogText = logTextBox.text!
-            let newLogText = "\(diceLabel): \(diceRollResult)\n" + currentLogText
-            logTextBox.text = newLogText
+        var timeSlice = 0.0
+        for tick in 1...tickEnd {
+            
+            timeSlice += Double(tick) / (Double(tickEnd) * 2.5)
+            
+            self.delay(timeSlice, closure: {
+                dispatch_async(dispatch_get_main_queue()){
+                    var temp = Int(arc4random_uniform(UInt32(numberOfDiceSides))) + 1
+
+                    while temp == diceRollResult {
+                        temp = Int(arc4random_uniform(UInt32(numberOfDiceSides))) + 1
+                    }
+                    diceRollResult = temp
+                    
+                    if tick == self.tickEnd {
+                        
+                        self.currentRollDisplayLabel.text = String(diceRollResult)
+                        
+                        self.runningSum += diceRollResult
+                        
+                        self.sumLabel.text = "Sum: " + String(self.runningSum)
+                        
+                        if let diceLabel = sender.titleLabel?.text! {
+                            let currentLogText = self.logTextBox.text!
+                            let newLogText = "\(diceLabel): \(diceRollResult)\n" + currentLogText
+                            self.logTextBox.text = newLogText
+                        }
+                    }
+                    else {
+                        self.currentRollDisplayLabel.text = String(diceRollResult)
+                    }
+                    
+                }
+            })
+            
         }
-        
-        
+    
     }
     
+
 
     // function to clear out the log and main roll display
     @IBAction func clearLogButtonPressed(sender: UIButton) {
@@ -88,7 +112,6 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         currentRollDisplayLabel.text = "~"
         logTextBox.text = "--------"
         sumLabel.text = "Sum: "
-
     }
     
     @IBAction func addBreakInLog(sender: UIButton) {
@@ -106,21 +129,8 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         currentRollDisplayLabel.text = "~"
     }
     
-    // function that randomly displays numbers from fast to slow
-    func spinTheWheel(max: Int) {
-        var timeSlice = 1.0
-        for tick in 1...10 {
-            
-            timeSlice = pow(2.0, Double(tick))
-            
-            delay(timeSlice / pow(2.0, 10.0)) {
-                dispatch_async(dispatch_get_main_queue()){
-                    self.currentRollDisplayLabel.text = String(Int(arc4random_uniform(UInt32(max))) + 1)
-                }
-            }
-        }
-    }
     
+    // used to delay the running main thread
     func delay(delay: Double, closure:()->()) {
         dispatch_after(
             dispatch_time(
